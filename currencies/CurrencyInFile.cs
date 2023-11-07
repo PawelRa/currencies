7f5068f69c6131493316e0101ca47a8b77cee74d
PawelRa <pawelrat@poczta.onet.pl>
2023-11-07 17:54:43 +0100

    Obsługa pliku

--- currencies/CurrencyInFile.cs
+++ currencies/CurrencyInFile.cs
@@ -1,115 +1,49 @@
-﻿//namespace currencies
-//{
-//    public class CurrencyInFile : CurrencyBase
-//    {
-//        private const string euroFile = "euro.txt";
-//        private const string dollarFile = "dollar.txt";
-//        private const string englishPoundFile = "pound.txt";
+﻿namespace currencies
+{
+    public class CurrencyInFile : CurrencyBase
+    {
+        private string FileName;
 
-//        public CurrencyInFile(string name) : base(name)
-//        {
-//        }
+        public CurrencyInFile(string name) : base(name)
+        {
+            FileName = this.Name + ".TXT";
+        }
 
-//        public override void AddCurrency(string name, double exchangeRate)
-//        {
-//            switch (name.ToUpper())
-//            {
-//                case "EUR":
-//                    using (var euroWriter = File.AppendText(euroFile))
-//                    {
-//                        euroWriter.WriteLine(exchangeRate);
-//                    }
-//                    break;
-//                case "USD":
-//                    using (var dollarWriter = File.AppendText(dollarFile))
-//                    {
-//                        dollarWriter.WriteLine(exchangeRate);
-//                    }
-//                    break;
-//                case "GBP":
-//                    using (var poundWriter = File.AppendText(englishPoundFile))
-//                    {
-//                        poundWriter.WriteLine(exchangeRate);
-//                    }
-//                    break;
-//                default:
-//                    throw new Exception("Incorrect name");
-//            }
-//        }
+        public override void AddCurrency(double exchangeRate)
+        {
 
-//        public override Statistics GetDollarStatistics()
-//        {
-//            var statistics = new Statistics("USD");
-//            if (File.Exists(dollarFile))
-//            {
-//                using (var reader = File.OpenText(dollarFile))
-//                {
-//                    var line = reader.ReadLine();
+            using (var fileWriter = File.AppendText(FileName))
+            {
+                fileWriter.WriteLine(exchangeRate);
+            }
+        }
 
-//                    while (line != null)
-//                    {
-//                        var number = double.Parse(line);
-//                        statistics.AddCurrency(number);
-//                        line = reader.ReadLine();
-//                    }
-//                }
-//            }
-//            return statistics;
-//        }
+        public override Statistics GetStatistics()
+        {
+            var statistics = new Statistics(FileName);
+            if (File.Exists(FileName))
+            {
+                using (var reader = File.OpenText(FileName))
+                {
+                    var line = reader.ReadLine();
 
-//        public override Statistics GetEuroStatistics()
-//        {
-//            var statistics = new Statistics("EUR");
-//            if (File.Exists(euroFile))
-//            {
-//                using (var reader = File.OpenText(euroFile))
-//                {
-//                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        var number = double.Parse(line);
+                        statistics.AddCurrency(number);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            return statistics;
+        }
 
-//                    while (line != null)
-//                    {
-//                        var number = double.Parse(line);
-//                        statistics.AddCurrency(number);
-//                        line = reader.ReadLine();
-//                    }
-//                }
-//            }
-//            return statistics;
-//        }
-
-//        public override Statistics GetPoundStatistics()
-//        {
-//            var statistics = new Statistics("GBP");
-//            if (File.Exists(englishPoundFile))
-//            {
-//                using (var reader = File.OpenText(englishPoundFile))
-//                {
-//                    var line = reader.ReadLine();
-
-//                    while (line != null)
-//                    {
-//                        var number = double.Parse(line);
-//                        statistics.AddCurrency(number);
-//                        line = reader.ReadLine();
-//                    }
-//                }
-//            }
-//            return statistics;
-//        }
-
-//        public void EuroFileDelete()
-//        {
-//            File.Delete(euroFile);
-//        }
-
-//        public void DollarFileDelete()
-//        {
-//            File.Delete(dollarFile);
-//        }
-
-//        public void PoundFileDelete()
-//        {
-//            File.Delete(englishPoundFile);
-//        }
-//    }
-//}
+        public void DeleteFile()
+        {
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+        }
+    }
+}
--- currencies/Program.cs
+++ currencies/Program.cs
@@ -4,13 +4,16 @@ Console.WriteLine("Witamy w programie do analizy kursu walut");
 Console.WriteLine("========================================");
 Console.WriteLine();
 
-//var currencyInFile = new CurrencyInFile();
 bool firstStepInMemory = false;
 
 Console.WriteLine("Wybierz metodę przechowywania danych");
 Console.WriteLine("Pamięć wciśnij 'P' lub dowolny klawisz dla zapisu na dysku");
 string firstStep = Console.ReadLine().ToUpper();
 bool continueProgram = true;
+
+CurrencyInFile euroInFile = new CurrencyInFile("EUR");
+CurrencyInFile dollarInFile = new CurrencyInFile("USD");
+CurrencyInFile poundInFile = new CurrencyInFile("GBP");
 
 if (firstStep == "P")
 {
@@ -27,6 +30,9 @@ else
     if (deleteFile.ToUpper() == "R")
     {
         //TODO: obsługa kasowania pliku
+        euroInFile.DeleteFile();
+        dollarInFile.DeleteFile();
+        poundInFile.DeleteFile();
     }
     WhileCurrencies(firstStepInMemory);
 }
@@ -117,13 +123,85 @@ void WhileCurrencies(bool choice)
             Console.WriteLine("Podsumowanie kursów funta brytyjskiego");
             Console.WriteLine($"Minimalny kurs = {poundStatistics.Min}, maksymalny kurs = {poundStatistics.Max}, średni kurs = {poundStatistics.Average}");
         }
-
-
-
     }
     else
     {
-        //TODO: OBsługa programu na plikach
+        //TODO: Obsługa programu na plikach
+        while (continueProgram)
+        {
+            Console.WriteLine("Wybierz walutę");
+            Console.WriteLine("EURO wybierz 'E', Dolar wybierz 'D', Funt szterling wybierz 'F'. Wyjście dowolny klawisz");
+
+            string currenciesChoice = Console.ReadLine().ToUpper();
+            if (currenciesChoice != "E" && currenciesChoice != "D" && currenciesChoice != "F")
+            {
+                continueProgram = false;
+                break;
+            }
+            else
+            {
+                while (continueProgram)
+                {
+                    Console.WriteLine("Wpisz aktualny kurs, wciśnij 'Z' by zmienić walutę lub 'Q' by wyjść");
+                    string tempValue = Console.ReadLine();
+
+                    if (tempValue.ToUpper() == "Z") { break; }
+
+                    if (tempValue.ToUpper() == "Q")
+                    {
+                        continueProgram = false;
+                        break;
+                    }
+
+                    double value = 0;
+                    try
+                    {
+                        value = Convert.ToDouble(tempValue);
+                    }
+                    catch { Console.WriteLine("Zła wartość"); }
+
+                    if (currenciesChoice == "E")
+                    {
+                        euroInFile.AddCurrency(value);
+                    }
+                    else if (currenciesChoice == "D")
+                    {
+                        dollarInFile.AddCurrency(value);
+                    }
+                    else if (currenciesChoice == "F")
+                    {
+                        poundInFile.AddCurrency(value);
+                    }
+                }
+            }
+        }
+
+        var euroStatistics = euroInFile.GetStatistics();
+        var dollarStatistics = dollarInFile.GetStatistics();
+        var poundStatistics = poundInFile.GetStatistics();
+
+        Console.WriteLine("Koniec programu");
+        Console.WriteLine();
+
+        if (euroStatistics.Count >= 1)
+        {
+            Console.WriteLine("Podsumowanie kursów EURO");
+            Console.WriteLine($"Minimalny kurs = {euroStatistics.Min}, maksymalny kurs = {euroStatistics.Max}, średni kurs = {euroStatistics.Average}");
+            Console.WriteLine();
+        }
+
+        if (dollarStatistics.Count >= 1)
+        {
+            Console.WriteLine("Podsumowanie kursów USD");
+            Console.WriteLine($"Minimalny kurs = {dollarStatistics.Min}, maksymalny kurs = {dollarStatistics.Max}, średni kurs = {dollarStatistics.Average}");
+            Console.WriteLine();
+        }
+
+        if (poundStatistics.Count >= 1)
+        {
+            Console.WriteLine("Podsumowanie kursów funta brytyjskiego");
+            Console.WriteLine($"Minimalny kurs = {poundStatistics.Min}, maksymalny kurs = {poundStatistics.Max}, średni kurs = {poundStatistics.Average}");
+        }
     }
 }
 
